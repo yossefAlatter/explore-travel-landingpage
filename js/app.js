e5e8b5ebe4aa935b94d06d9d0b807636b3e7ce63
yossefAlatter <yossefalatter@gmail.com>
2021-04-20 19:51:41 +0200

    media for different screens

--- js/app.js
+++ js/app.js
@@ -1,48 +1,102 @@
-//== navbar scripts ==//
+//== adding item to navbar dynamically ==//
 //== start ==//
 
-//== variable we need ==//
+//== handle elements that we need from DOM ==//
+//== start ==//
+const sections = document.querySelectorAll('section');
+const navList = document.querySelector('.nav-list');
+const fragmentDOM = document.createDocumentFragment();
+//== end =//
+
+//== function we need ==//
 //== start ==//
 
+//== create navlink ==//
+function createNavLink(el){
+    let navLink = document.createElement('li');
+    navLink.classList.add('nav-link')
+    navLink.textContent = el.getAttribute('data-nav');
+    fragmentDOM.appendChild(navLink);
+}
+
+//== end ==//
+
+//== creating nav list item according to sections in page ==//
+//== start ==//
+sections.forEach(createNavLink);
+navList.appendChild(fragmentDOM);
+//== end ==//
+
+//== end ==//
+
+//=================================================================================================//
+
+//== scroll action to in certain section ==//
+//== start ==//
+
+//== handle elements that we need from DOM ==//
+//== start ==//
+const navLinks =document.querySelectorAll('.nav-link');
+//== end ==//
+
+//== event we needs ==//
+//== start ==//
+
+//== navlinks event ==//
+navLinks.forEach((el, ind)=>{
+    el.addEventListener('click',()=>{
+        console.log('55')
+        sections[ind].scrollIntoView({behavior: "smooth", block: "start"});
+    });
+});
+
+//== end ==//
+
+//== end ==//
+
+//=================================================================================================//
+
+
+//== controlling navbar in mobile media ==//
+//== start ==//
+
+//== handle elements that we need from DOM  ==//
+//== start ==//
 const toggleBtn = document.getElementById('toggle');
 const navbarMenu = document.getElementById('nav-menu');
 const closeBtn = document.getElementById('close');
-
 //== end ==//
 
 //== functions we need ==//
 //== start ==//
 
-
-
-//== end ==//
-
-//== events we need ==//
-//== start ==//
-
-//== toggle btn event ==//
-toggleBtn.addEventListener('click',()=>{
+//== function to open navbar ==//
+function openNavbar(){
     navbarMenu.style.display = "flex" ;
     navbarMenu.animate([
         {
             transform: 'translateX(100%)',
             opacity: '0'
-    },
+        }
+    ,
         {
             transform: 'translateX(0)',
             opacity: '1'
         }
-    ],{
+    ],
+    {
         duration: 400
-    })
-})
+    });
+    toggleBtn.style.display = "none"
+}
 
-//== close btn event ==//
-closeBtn.addEventListener('click', ()=>{
+//== function to close navbar ==//
+function closeNavbar(){
     navbarMenu.animate([
         {
             opacity: '1'
-    },
+        }
+    ,
         {
             opacity: '0'
         }
@@ -52,8 +106,22 @@ closeBtn.addEventListener('click', ()=>{
     setTimeout(()=>{
         navbarMenu.style.display = "none" ;
     },100);
-})
+    toggleBtn.style.display = "block";
+}
+
+//== end ==//
+
+//== events we need ==//
+//== start ==//
+
+//== toggle btn event ==//
+toggleBtn.addEventListener('click', openNavbar);
+
+//== close btn event ==//
+closeBtn.addEventListener('click', closeNavbar);
 
 //== end ==//
 
 //== end ==//
+
+//=================================================================================================//
