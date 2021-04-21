0268b48c64a8133548371752b5d2171e779185be
yossefAlatter <yossefalatter@gmail.com>
2021-04-21 16:51:31 +0200

    end2

--- js/app.js
+++ js/app.js
@@ -218,7 +218,7 @@ window.addEventListener('scroll',(e)=>{
     handleScroll();
     indicateActive();
     visibilityScrollTop();
-    visibilityNavbar()
+    visibilityNavbar();
 });
 
 //== scroll btn click event ==//
